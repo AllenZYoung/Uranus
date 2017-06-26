e3e31c299451f7ab00dc823eb0cfb378de7d836d
yangjiawei <yangjiawei@xiaomi.com>
2017-06-26 23:01:18 +0800

    add some teacher page
    Signed-off-by: yangjiawei <yangjiawei@xiaomi.com>

--- app/teacher/urls.py
+++ app/teacher/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     url(r'^edit_course/', views.edit_course, name='edit_course'),
     url(r'^edit_homework/', views.edit_homework, name='edit_homework'),
     url(r'^course_info/', views.course_info, name='course_info'),
-    url(r'^course_edit_form/(?P<pk>[0-9]+)$',generic_views.CourseUpdate.as_view()),
+    url(r'^course_edit_form/(?P<pk>[0-9]+)$', generic_views.CourseUpdate.as_view()),
+    url(r'^upload_file/', views.upload_file, name='upload_file'),
+    url(r'^resources/', views.resources, name='resources'),
+    url(r'^homework/', views.homework, name='homework'),
+
 ]
--- app/teacher/views.py
+++ app/teacher/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from app.models import *
+from app.teacher.forms import *
+from app.teacher.utils import *
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -9,11 +12,11 @@ from app.models import *
 def index(request):
     user = request.user
     # 教师首页默认显示该教师的所有课程
-    enrolls = Enroll.objects.filter(user__username=user.username)
+    enrolls = Enroll.objects.filter(user__username=user.username, user__role='teacher')
     courses = []
     for enroll in enrolls:
         courses.append(enroll.course)
-    teacher = User.objects.filter(username=user.username)
+    teacher = User.objects.get(username=user.username)
     return render(request, 'teacher/teacher_index.html', {'courses': courses, 'teacher': teacher})
 
 
@@ -37,11 +40,48 @@ def course_info(request):
     course_id = request.GET.get('course_id', None)
     if course_id is None:
         return HttpResponse('course_id=None')
-    course = Course.objects.filter(id=course_id)
+    course = Course.objects.get(id=course_id)
     user = request.user
-    enrolls=Enroll.objects.filter(course_id=course_id)
-    teachers=[]
+    enrolls = Enroll.objects.get(course_id=course_id)
+    teachers = []
+    students = []
     for enroll in enrolls:
-        teachers.append(enroll.user)
-    teacher = User.objects.filter(username=user.username)
-    return render(request, 'teacher/course_info.html', {'course': course, 'teachers': teachers,'teacher':teacher})
+        if enroll.user.role == 'student':
+            students.append(enroll.user)
+        elif enroll.user.role == 'teacher':
+            teachers.append(enroll.user)
+    teacher = User.objects.get(username=user.username)
+    return render(request, 'teacher/course_info.html',
+                  {'course': course, 'teachers': teachers, 'teacher': teacher, 'students': students})
+
+
+@login_required(login_url='app:login')
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request)
+            return HttpResponse('upload file success')
+        else:
+            return HttpResponse('form is not valid')
+    else:
+        return HttpResponse('upload file is empty!')
+
+
+# todo select all resourses for a course
+@login_required(login_url='app:login')
+def resources(request):
+    course_id = request.GET.get('course_id', None)
+    if course_id is None:
+        return HttpResponse('course_id=None')
+    course = Course.objects.get(id=course_id)
+    user = request.user
+    upload_file_form = UploadFileForm()
+    teacher = User.objects.get(username=user.username)
+    return render(request, 'teacher/resources.html',
+                  {'course': course, 'teacher': teacher, 'upload_file_form': upload_file_form})
+
+
+@login_required(login_url='app:login')
+def homework(request):
+    return HttpResponse('homework')
