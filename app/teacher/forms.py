883861abb74f94133007c8137e5a2262eb946bac
yangjiawei <yangjiawei@xiaomi.com>
2017-06-27 17:32:57 +0800

    add some code
    Signed-off-by: yangjiawei <yangjiawei@xiaomi.com>

--- app/teacher/forms.py
+++ app/teacher/forms.py
@@ -8,19 +8,24 @@ class UploadFileForm(forms.Form):
 
 
 class HomeworkForm(forms.Form):
-    content=forms.CharField(label='作业内容')
-    proportion=forms.FloatField(label='总分占比',min_value=0,max_value=1)
-    submits=forms.IntegerField(label='最大提交次数',min_value=1)
-    startTime=forms.DateTimeField(label='开始日期')
-    endTime=forms.DateTimeField(label='截止日期')
-    attachment=forms.FileField(label='上传附件')
+    content = forms.CharField(label='作业内容')
+    proportion = forms.FloatField(label='总分占比', min_value=0, max_value=1)
+    submits = forms.IntegerField(label='最大提交次数', min_value=1)
+    startTime = forms.DateTimeField(label='开始日期')
+    endTime = forms.DateTimeField(label='截止日期')
+    attachment = forms.FileField(label='上传附件', required=False)
+
+    def __init__(self, content, proportion, submits, startTime, endTime):
+        super().__init__()
+        self.content = content
+        self.proportion = proportion
+        self.submits = submits
+        self.startTime = startTime
+        self.endTime = endTime
 
 
-
-
-#comment and score form
+# comment and score form
 class CommentAndScoreForm(forms.Form):
     homework_id = forms.CharField()
     score = forms.IntegerField(help_text='score')
     comment = forms.CharField(help_text='comment')
-
