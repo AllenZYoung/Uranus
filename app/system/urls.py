d4bd1454859c520e8193b890779accfa51b81aa2
zxl <fjzxl1@163.com>
2017-06-27 09:17:52 +0800

    add batch upload students&teachers

--- app/system/urls.py
+++ app/system/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'system'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^uploadStudents$', views.upload_students, name='uploadStudent'),
+    url(r'uploadTeachers$',views.upload_teachers, name='uploadTeachers'),
 ]
