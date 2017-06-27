b2fb83600f75656553396e47f8c4a7801cf75da3
zxl <fjzxl1@163.com>
2017-06-27 19:46:54 +0800

    add_term&course fix

--- app/system/urls.py
+++ app/system/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^uploadTeachers$', views.upload_teachers, name='uploadTeachers'),
     url(r'^createTerm$', views.create_term, name='createTerm'),
     url(r'^createCourse$', views.create_course, name='createCourse'),
+    url(r'^showTerm$', views.show_term, name='showTerm'),
 ]
