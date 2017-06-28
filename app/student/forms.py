d40af27073353896ca0f55e7cb85089e72774875
AllenZYoung <allenzyoungazy@gmail.com>
2017-06-28 11:10:31 +0800

    Student Contribution Struggle

--- app/student/forms.py
+++ app/student/forms.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 from django import forms
-
+from .models import *
+from django.forms import ModelForm
 #define your custom forms here
+
+
+class ContributionForm(ModelForm):
+    class Meta:
+        model = Member
+        fields = ['user', 'role', 'contribution']
--- app/student/views.py
+++ app/student/views.py
@@ -4,30 +4,78 @@ from django.contrib.auth.decorators import login_required
 from app.models import File, User, Work, WorkMeta
 from django.conf import settings
 import os
+from .forms import ContributionForm
+from .models import Member, User, Team
 from django.shortcuts import get_object_or_404
 from . import utils
-from .utils import set_members_evaluations
-#TODO 学生登录和注销（一般而言，要与教师、教务统一）
 
+
+# from .utils import set_members_evaluations
 
 
 # Create your views here.
+
+@login_required(login_url='app:login')
 def index(request):
-    return render(request,'student/course_student.html')
+    return render(request, 'student/course_student.html')
 
-#@login_required(login_url='app:login')
-def member_evaluation(request): # （团队负责人）学生的成员评价页面
+
+'''
     if request.method == 'GET':
-        student_id = request.GET.get('student_id',None)
-        student = User.objects.get(username=student_id)
-        set_members_evaluations(student)
-    return HttpResponse('Member Evaluation here.')
+        course_id = request.GET.get('course_id', None)
+        course = get_object_or_404(Course, id=course_id)
+        homework_form = HomeworkForm()
+        return render(request, 'teacher/create_homework.html', {'homework_form': homework_form, 'course': course})
+    else:
+        course_id = request.POST.get('course_id', None)
+        course = get_object_or_404(Course, id=course_id)
+        form = HomeworkForm(request.POST)
+        if form.is_valid():
+            add_homework(form, course_id, request.user.username)
+            return HttpResponseRedirect('/homework/')
+        else:
+            error_message = '数据不合法'
+            return render(request, 'teacher/create_homework.html',
+                          {'homework_form': form, 'course': course, 'error_message': error_message})'''
 
-#@login_required(login_url='app:login')
-def work_submit(request): # （团队负责人）学生的作业提交页面
+
+# todo 该函数很不完善
+@login_required(login_url='app:login')
+def member_evaluation(request):  # （团队负责人）学生的团队管理，即成员评价页面
+    if request.method == 'GET':  # 显示所有成员及其贡献度（包括队长自己）
+        student_id = request.user
+        member_model = Member.objects.filter(user__username__contains=student_id).first()
+        team = member_model.team
+        member_list = Member.objects.filter(team=team)
+        contribution_form = ContributionForm()
+        return render(request, ''
+                               'student/student_member_contribution.html',
+                      {'contribution_form': contribution_form, 'team': team, 'member_list': member_list})
+    elif request.method == 'POST':  # 改变每个人的权重（贡献度）
+        student_id = request.POST.get('name', None)
+        f = ContributionForm(request.POST)
+        # 此处表单没写好，注意。@果冻
+        if f.is_valid():
+            # set_members_evaluations(student_id)
+            member_model = Member.objects.filter(user__username__contains=student_id)
+            team = Team.objects.filter(member_model.team).first()
+            member_list = Member.objects.filter(team=team)
+            for member in member_list:
+                if member.user == f.cleaned_data['user']:
+                    member.contribution = f.cleaned_data['contribution']
+                    member.save()
+        else:
+            error_message = 'Some error here!'
+            return render(request, 'student/student_member_contribution.html',
+                          {'contribution_form': contribution_form, 'error_message': error_message})
+
+
+# @login_required(login_url='app:login')
+def work_submit(request):  # （团队负责人）学生的作业提交页面
     return HttpResponse('Submit your group\'s homework here.')
 
-#@login_required(login_url='app:login')
+
+# @login_required(login_url='app:login')
 def view_resources(request):
     files = File.objects.all()
     file_meta = []
@@ -38,9 +86,9 @@ def view_resources(request):
                           'date': file.time})
     return render(request, 'student/resources.html', {'file_meta': file_meta, })
 
-#@login_required(login_url='app:login')
-def download(request):
 
+# @login_required(login_url='app:login')
+def download(request):
     def read_file(fn, buf_size=262144):
         f = open(fn, 'rb')
         while True:
@@ -58,18 +106,19 @@ def download(request):
     response['Content-Disposition'] = 'attachment;filename="{0}"'.format(file_name)
     return response
 
-#@login_required(login_url='app:login')
+
+# @login_required(login_url='app:login')
 def view_admitted_work(request):
     team_id = 1
     course_id = 1
     submittings = utils.get_submittings(team_id, course_id)
     return render(request, 'student/admitted_work.html', {'submitted': submittings['submitted'],
-                                                          'unsubmitted': submittings['unsubmitted'],})
+                                                          'unsubmitted': submittings['unsubmitted'], })
 
 
 # Added by kahsolt 2017-06-27
-#@login_required(login_url='app:login')
+# @login_required(login_url='app:login')
 def workView(request):
     wid = request.content_params.get('id')
     work = Work.objects.filter(id=wid)
-    return render(request, 'student/work.html', {'work': work,})
+    return render(request, 'student/work.html', {'work': work, })
