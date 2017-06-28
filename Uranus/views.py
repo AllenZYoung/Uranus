c1c9da153316907eabbbac27e900c64b464b4e0c
kahsolt <kahsolt@qq.com>
2017-06-28 11:21:54 +0800

    SAMPLE DB!!

--- Uranus/views.py
+++ Uranus/views.py
@@ -4,7 +4,13 @@ from app.test.sampleDB import sampleDB
 
 
 def index(request):
-    return HttpResponse('Uranus Project')
+    str='<h1>Uranus Project</h1>'
+    str+='<br/><div>'
+    str+='<p><a href="test/sampleDB">Install Sample DB</a><p>'
+    str+='<p>请先以普通py脚本的方式运行/Uranus/META/sampleDB_base.py</p>'
+    str+='<p>如果数据库崩了请运行/Uranus/META/reinit.sql</p>'
+    str+='</div>'
+    return HttpResponse(str)
 
 
 # Added by kahsolt
