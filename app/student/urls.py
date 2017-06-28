32cc90629428a748cb1617a84b856f8d45c717f2
JellyWong <wanggd_buaa@163.com>
2017-06-28 14:47:01 +0800

    add viewing work details

--- app/student/urls.py
+++ app/student/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^work_submit$',views.work_submit, name='work_submit'), # 团队负责人的作业提交页面
     url(r'^resources$', views.view_resources, name='resources'),  # 资源列表页面
     url(r'^s', views.download, name='download'),  # 资源下载链接
-    url(r'^works', views.view_admitted_work, name='admitted_work'),  # 资源下载链接
-    url(r'^work', views.workView, name='work'),  # 资源下载链接
+    url(r'^works', views.view_admitted_work, name='admitted_work'),  # 查看作业提交情况
+    url(r'^work', views.workView, name='work'),  # 查看作业详情
 ]
