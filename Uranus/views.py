6b6867da9b565d092d7beaab8adb1ab7d25bb5e2
kahsolt <kahsolt@qq.com>
2017-06-28 11:39:27 +0800

    adjust sample DB

--- Uranus/views.py
+++ Uranus/views.py
@@ -6,9 +6,10 @@ from app.test.sampleDB import sampleDB
 def index(request):
     str='<h1>Uranus Project</h1>'
     str+='<br/><div>'
-    str+='<p><a href="test/sampleDB">Install Sample DB</a><p>'
-    str+='<p>请先以普通py脚本的方式运行/Uranus/META/sampleDB_base.py</p>'
-    str+='<p>如果数据库崩了请运行/Uranus/META/reinit.sql</p>'
+    str+='<h3>安装样例数据库</h3>'
+    str+='<p style="color: red;">请先以普通py脚本的方式运行/Uranus/META/sampleDB_base.py<br/>'
+    str+='如果数据库崩了请运行/Uranus/META/reinit.sql</p>'
+    str+='<p><a href="test/sampleDB">[[Start Install Sample DB]]</a><p>'
     str+='</div>'
     return HttpResponse(str)
 
