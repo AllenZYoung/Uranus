10d88df550d2aa001149d9e8fce21c7d02b73e2e
zxl <fjzxl1@163.com>
2017-06-28 20:58:02 +0800

    教务页面部分完成

--- app/system/urls.py
+++ app/system/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'system'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^term$', views.index, name='term'),
+    url(r'^course$', views.index_course, name='index_course'),
     url(r'^uploadStudents$', views.upload_students, name='uploadStudent'),
     url(r'^uploadTeachers$', views.upload_teachers, name='uploadTeachers'),
     url(r'^createTerm$', views.create_term, name='createTerm'),
@@ -14,4 +16,5 @@ urlpatterns = [
     url(r'^editTerm$', views.edit_term, name='editTerm'),
     url(r'editCourse$', views.edit_course, name='editCourse'),
     url(r'^loadStudents$', views.load_student, name='loadCourse'),
+    url(r'^loadTeacher$', views.load_teacher, name='loadTeacher'),
 ]
