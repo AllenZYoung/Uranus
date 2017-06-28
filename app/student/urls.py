f00cadeee58f9f1787400e8e2b840f143c39146f
AllenZYoung <allenzyoungazy@gmail.com>
2017-06-28 16:57:26 +0800

    New renders

--- app/student/urls.py
+++ app/student/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     url(r'^member_evaluation$', views.member_evaluation, name='member_evaluation'), # 团队负责人的团队成员评价页面，可设置贡献度等
     url(r'^work_submit$',views.work_submit, name='work_submit'), # 团队负责人的作业提交页面
     url(r'^resources$', views.view_resources, name='resources'),  # 资源列表页面
-    url(r'^s', views.download, name='download'),  # 资源下载链接
-    url(r'^works', views.view_admitted_work, name='admitted_work'),  # 查看作业提交情况
-    url(r'^work', views.workView, name='work'),  # 查看作业详情
+    url(r'^s$', views.download, name='download'),  # 资源下载链接
+    url(r'^works$', views.view_admitted_work, name='admitted_work'),  # 查看作业提交情况
+    url(r'^work$', views.workView, name='work'),  # 查看作业详情
+    url(r'^workpage$', views.workRoot, name='workpage'),  # 查看作业详情
+
 ]
