16dc919f8bfd0c7de1b3ad6edc9422bfb5ae6961
BuildTools <kahsolt@qq.com>
2017-06-29 10:41:41 +0800

    更新gitignore，增加static路径目录

--- Uranus/settings.py
+++ Uranus/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "uploads"),
+    os.path.join(BASE_DIR, "downloads"),
 ]
 
 # Support ImageField/FileField
