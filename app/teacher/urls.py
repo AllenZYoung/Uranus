73f0590f90468c9c02c859ac9be00f6ba0721d3b
yangjiawei <yangjiawei@xiaomi.com>
2017-07-01 11:15:11 +0800

    add team_memebers page
    Signed-off-by: yangjiawei <yangjiawei@xiaomi.com>

--- app/teacher/urls.py
+++ app/teacher/urls.py
@@ -32,6 +32,5 @@ urlpatterns = [
     url(r'^team_manage/', views.team_manage, name='team_manage'),
     url(r'^teams/',views.teams,name='teams'),
     url(r'^team_memebers/',views.team_members,name='team_members'),
-    url(r'^adjust_team$',views.adjust_team,name='adjust_team')
-
+    url(r'^adjust_team/',views.adjust_team,name='adjust_team'),
 ]
